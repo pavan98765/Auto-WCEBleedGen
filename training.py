b9daef15277f66af3f77e89286680f7a0f5535ea
pavan98765 <pavancshekar60@gmail.com>
2023-10-01 14:59:57 +0530

    edits

--- training.py
+++ training.py
@@ -12,7 +12,6 @@ from ultralytics import YOLO
 # Specify the location of your dataset and data.yaml file
 dataset_location = "/path/to/your/dataset"
 data_yaml_path = "/path/to/your/data.yaml"
-data_yaml_path = "C:/Users/Jarvis/Desktop/BTP_wce/#wce_detection/notebooks/datasets/wce_clean_train_2.0-1/data.yaml"
 
 # Initialize the YOLOv8 model
 model = YOLO(model="yolov8s.pt")
@@ -23,11 +22,11 @@ model.train(
     epochs=50,
     imgsz=800,
     data=data_yaml_path,
+    plots=True,  # Optional: Enable or disable training plots
     # weights="/path/to/pretrained/weights",  # Optional: Provide pretrained weights
     # batch_size=16,  # Optional: Adjust batch size as needed
     # device="0",  # Optional: Specify GPU device if you have multiple GPUs
     # project="your_project_name",  # Optional: Specify a project name
     # name="your_run_name",  # Optional: Specify a run name
     # save_dir="/path/to/save/directory",  # Optional: Specify a directory to save checkpoints and logs
-    plots=True,  # Optional: Enable or disable training plots
 )
